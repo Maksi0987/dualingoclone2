e31e178767e99f34a64ae02573e652ef0fac0007
Dima <dimasic162@gmail.com>
2026-07-09 20:53:32 +0300

    реалізовано базовий макет та інтерфейс сторінки навчання (Learn)
    Створено LearnViewModel та додано тестові дані в контролер
    
    Налаштовано головний _Layout.cshtml з інтеграцією Bootstrap 5.3
    
    Розроблено 3-колонкову структуру сайту (навігація, змійка уроків, квести)
    
    Додано кастомні стилі для відображення картки уроків у стилі Duolingo

---
+++ languio/Models/LanguageCourse.cs
@@ -0,0 +1,10 @@
+﻿namespace Languio.Models
+{
+    public class LanguageCourse
+    {
+        public int Id { get; set; }
+        public string LanguageCode { get; set; }
+        public string Title { get; set; }
+        public List<LanguageLesson> Lessons { get; set; } = new();
+    }
+}
---
+++ languio/Models/LanguageQuestion.cs
@@ -0,0 +1,23 @@
+﻿namespace Languio.Models
+{
+    public enum QuestionType
+    {
+        MultipleChoice,
+        FillInTheBlank,
+        TrueFalse,
+        Matching,
+        ShortAnswer
+    }
+    public class LanguageQuestion
+    {
+        public int Id { get; set; }
+        public QuestionType Type{ get; set; }
+        public int Difficulty { get; set; }
+        public string QuestionText { get; set; }
+        public string CorrectAnswer { get; set; }
+        public string OptinsRaw { get; set; }
+
+        public int LanguageLessonId { get; set; }
+        public LanguageLesson LanguageLesson { get; set; }
+    }
+}
